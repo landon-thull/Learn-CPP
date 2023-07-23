7dce5f99ad83f1e0a68b3de062e3bc95a6d7b266
Landon Thull <landonthull.tx@gmail.com>
2023-07-23 17:57:11 -0500

    add section 14_classes_and_objects

---
+++ 14_classes_and_objects/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+class Book
+{
+public:
+  string title;
+  string author;
+  int pages;
+};
+
+int main()
+{
+  Book book1;
+
+  book1.title = "Harry Potter";
+  book1.author = "JK Rowling";
+  book1.pages = 341;
+
+  Book book2;
+  book2.title = "Lord of the Rings";
+  book2.author = "Tolkein";
+  book2.pages = 700;
+
+  cout << book1.title << endl;
+  cout << book2.author << endl;
+
+  return 0;
+}
