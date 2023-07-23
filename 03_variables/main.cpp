b69686744021169cd8bf0fc196fc3b98d4d6cdbd
Landon Thull <landonthull.tx@gmail.com>
2023-07-23 15:24:53 -0500

    Add section 03_variables

---
+++ 03_variables/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  string characterName = "Landon";
+  int characterAge;
+  characterAge = 17;
+
+  cout << "There once was a man named " << characterName << endl;
+  cout << "He was " << characterAge << " years old" << endl;
+
+  characterName = "Garrett";
+
+  cout << "He liked the name " << characterName << endl;
+  cout << "But he did not like being " << characterAge << endl;
+
+  return 0;
+}
